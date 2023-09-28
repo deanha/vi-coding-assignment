a9e48ea4ffa117c2a45d42c87cb850b92274bb12
Dean Harel <anichego@gmail.com>
2023-09-29 00:53:23 +0300

    Movies endpoint

---
+++ src/controllers/movies.controller.js
@@ -0,0 +1,25 @@
+const catchAsync = require("../utils/catchAsync");
+const { movies, actors } = require("../../dataForQuestions");
+const APIError = require("../utils/APIError");
+const _ = require("lodash");
+const getMoviesInfo = require("../services/TMDBService/utils");
+
+const getMoviesByActor = catchAsync(async (req, res) => {
+  try {
+    const info = await getMoviesInfo(movies);
+    const infoByActorName = _.groupBy(info, "actorName");
+
+    const result = actors.reduce((accumulator, actor) => {
+      accumulator[actor] = (infoByActorName[actor] ?? []).map(
+        (info) => info.movieName,
+      );
+      return accumulator;
+    }, {});
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    throw APIError.internalServerError;
+  }
+});
+
+module.exports = { getMoviesByActor };
